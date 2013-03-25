791b15870d37c31ff260df1968562e301d36d3c4
Brian McCallister <brianm@skife.org>
2013-03-25 14:55:10 -0600

    add a class

---
+++ src/main/java/org/skife/jetty/v9/ThisPartIsNotJetty.java
@@ -0,0 +1,12 @@
+package org.skife.jetty.v9;
+
+import java.lang.String;
+
+/**
+ * I need to have some source, and some javadocs, or oss.sonsatype.org
+ * gets angry. This class exists to keep oss.s.o happy :-)
+ */
+public class ThisPartIsNotJetty
+{
+
+}
