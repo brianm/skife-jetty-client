917a4c5e7c39204978d480a657333b2e9a81221b
Brian McCallister <bmccallister@groupon.com>
2014-02-05 13:47:48 -0800

    make work with basepom

--- src/main/java/org/skife/jetty/v9/ThisPartIsNotJetty.java
+++ src/main/java/org/skife/jetty/v9/ThisPartIsNotJetty.java
@@ -1,7 +1,5 @@
 package org.skife.jetty.v9;
 
-import java.lang.String;
-
 /**
  * I need to have some source, and some javadocs, or oss.sonsatype.org
  * gets angry. This class exists to keep oss.s.o happy :-)
